bb880488d1a7c1a1f47e64075372a6d6923d9c59
xingshuyin <xing-shuyin@qq.com>
2025-09-28 11:59:44 +0800

    加速本地文件传输

--- example/client.py
+++ example/client.py
@@ -1,5 +1,6 @@
 from fastrpc import FastRpcClient
 from datetime import datetime
+import time
 
 c = FastRpcClient()
 
@@ -8,6 +9,22 @@ print(c.echo_float(1.0))
 print(c.echo_bool(True))
 print(c.echo_list([1, 2, 3]))
 print(c.echo_dict({"a": 1, "b": 2}))
-print(c.echo_bytes(b"123"))
+start = time.time()
+file = "/home/c/project/vam/back/media/videos/2025/9/4/1a892844-1ce4-4561-b016-48950c675e33.mp4"
+with open("output.mp4", "wb") as f:
+    f.write(c.echo_bytes(file))
+end = time.time()
+print(f"[方式1] 传路径，耗时：{end - start:.2f} 秒")
+
+
+start = time.time()
+with open(file, "rb") as f:
+    r = c.echo_bytes(f.read())
+    with open("output2.mp4", "wb") as f2:
+        f2.write(r)
+end = time.time()
+print(f"[方式2] 传内容，耗时：{end - start:.2f} 秒")
+
+
 print(c.echo_str("123"))
 print(c.echo_datetime(datetime.now()))
--- fastrpc/fastrpc.py
+++ fastrpc/fastrpc.py
@@ -10,6 +10,9 @@ import inspect
 import requests
 import os
 from datetime import datetime
+import tempfile
+
+localhosts = ("127.0.0.1", "localhost", "0.0.0.0")
 
 
 class FastRpc:
@@ -36,14 +39,24 @@ class FastRpc:
                             )
                             params[k] = await file_obj.read()
                         else:
-                            # 如果获取到的不是 UploadFile，可能是字符串或其他类型
-                            print(
-                                f"参数 {k} 的类型: {type(file_obj)}, 值: {str(file_obj)[:100]}..."
-                            )
-                            raise HTTPException(
-                                status_code=400,
-                                detail=f"参数 {k} 需要文件但获取到的是 {type(file_obj)}",
-                            )
+                            if self.host in localhosts:
+                                if os.path.exists(file_obj):
+                                    with open(file_obj, "rb") as f:
+                                        params[k] = f.read()
+                                else:
+                                    raise HTTPException(
+                                        status_code=400,
+                                        detail=f"参数 {k} 指定的文件路径不存在: {file_obj}",
+                                    )
+                            else:
+                                # 如果获取到的不是 UploadFile，可能是字符串或其他类型
+                                print(
+                                    f"参数 {k} 的类型: {type(file_obj)}, 值: {str(file_obj)[:100]}..."
+                                )
+                                raise HTTPException(
+                                    status_code=400,
+                                    detail=f"参数 {k} 需要文件但获取到的是 {type(file_obj)}",
+                                )
                     elif v.annotation is str:
                         params[k] = form[k]
                     elif v.annotation is int:
@@ -62,11 +75,22 @@ class FastRpc:
                         params[k] = form[k]
                 r = self.funcs[path](**params)  # 返回类型转换
                 if type(r) is bytes:
-                    return Response(
-                        content=r,
-                        media_type="application/octet-stream",  # 通用二进制类型
-                        headers={f"Content-Disposition": "attachment; filename={path}"},
-                    )
+                    if self.host in localhosts:
+                        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                            tmp_file.write(r)
+                            temp_path = tmp_file.name  # 文件不会自动删除
+                        return Response(
+                            content=temp_path,
+                            media_type="text/plain",
+                        )
+                    else:
+                        return Response(
+                            content=r,
+                            media_type="application/octet-stream",  # 通用二进制类型
+                            headers={
+                                f"Content-Disposition": "attachment; filename={path}"
+                            },
+                        )
                 if type(r) is bool:
                     return 1 if r else 0
                 return r
@@ -136,6 +160,8 @@ class FastRpc:
         return decorator
 
     def run(self, host="0.0.0.0", port=8000):
+        self.host = host
+        self.port = port
         uvicorn.run(self.app, host=host, port=port)
 
 
@@ -152,10 +178,15 @@ class FastRpcClient:
             params = {}
             files = {}
             params_list = list(self.funs[funcname]["params"].keys())
+            params_dict = self.funs[funcname]["params"]
             for i, v in enumerate(args):
                 if type(v) is str:
-                    if os.path.exists(v):
-                        files[params_list[i]] = (os.path.basename(v), open(v, "rb"))
+                    if os.path.exists(v) and params_list[i] == "bytes":
+                        if self.host in localhosts:
+                            params[params_list[i]] = v
+                        else:
+                            with open(v, "rb") as f:
+                                files[params_list[i]] = (os.path.basename(v), f)
                     else:
                         params[params_list[i]] = v
                 elif type(v) is bytes:
@@ -176,8 +207,12 @@ class FastRpcClient:
                     params[params_list[i]] = v
             for k, v in kwargs.items():  # 参数类型转换
                 if type(v) is str:
-                    if os.path.exists(v):
-                        files[k] = (os.path.basename(v), open(v, "rb"))
+                    if os.path.exists(v) and params_dict[k] == "bytes":
+                        if self.host in localhosts:
+                            params[k] = v
+                        else:
+                            with open(v, "rb") as f:
+                                files[k] = (os.path.basename(v), f)
                     else:
                         params[k] = v
                 elif type(v) is bytes:
@@ -201,6 +236,12 @@ class FastRpcClient:
             )
             r_type = self.funs[funcname]["return"]  # 返回类型还原
             if r_type == "bytes":
+                if self.host in localhosts:
+                    print(f"Received local file: {r.text}")
+                    with open(r.text, "rb") as f:
+                        content = f.read()
+                    os.remove(r.text)  # 删除临时文件
+                    return content
                 return r.content
             elif r_type == "str":
                 return r.text
